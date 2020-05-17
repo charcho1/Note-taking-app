9a9a8fc778995fa0111aa8f01375bbda5eb84ea0
g4m3r2 <choicharles31@gmail.com>
2020-05-17 07:27:32 -0700

    change

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,7 +2,7 @@ import React from 'react';
 const BlogContext = React.createContext();
 //BlogContext an obj which will move info from BPP to blog list
 
-export const BlogProvider({children}) => {
+export const BlogProvider ({children}) => {
     return <BlogContext.Provider>
         {children}
     </BlogContext.Provider>
